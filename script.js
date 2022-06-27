a982d149ed6fff1cc05b885f36fcfd081e6dd468
Nyame-Wolf <mumenyam@gmail.com>
2022-06-27 22:46:44 +0300

    Add book selectots and create booke methods

---
+++ script.js
@@ -0,0 +1,15 @@
+const bookForm = document.querySelector('.book-form');
+const bookContainer = document.querySelector('.book-container');
+const titleInput = bookForm['title']
+const authorInput = bookForm['author']
+
+const books = [
+]
+
+const addBook = (author, book) => { }
+
+const createBook = () => {
+    const bookDiv = document.createElement('div')
+
+
+}
