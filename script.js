bbdc6f15a677e282405103fc9de4c1d4ecc97783
Nyame-Wolf <mumenyam@gmail.com>
2022-06-28 00:16:45 +0300

    Add books to local storage

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const bookContainer = document.querySelector('.book-container');
 const titleInput = bookForm['title']
 const authorInput = bookForm['author']
 
-const books = [
-]
+const books = JSON.parse(localStorage.getItem("books")) || []
 
-const addBook = (author, book) => { }
+const addBook = (author, book) => {
+    books.push({
+        title,
+        author
+    });
+    localStorage.setItem("books", JSON.stringify(books))
+    return { title, author }
+}
 
-const createBook = ({title,author,button,hr}) => {
+const createBook = ({ title, author, button, hr }) => {
     const bookDiv = document.createElement('div');
     const bookTitle = document.createElement('p');
     const bookAuthor = document.createElement('p');
@@ -19,6 +25,20 @@ const createBook = ({title,author,button,hr}) => {
     bookAuthor.innerText = `${author}`
     removeBtn.innerText = `Remove`
 
-    bookDiv.append(bookTitle,bookAuthor,removeBtn,formHr);
+    bookDiv.append(bookTitle, bookAuthor, removeBtn, formHr);
     bookContainer.appendChild(bookDiv);
+}
+
+books.forEach(createBook)
+
+bookForm.onsubmit = (e) => {
+    e.preventDefault()
+
+    const newBook = addBook(
+        titleInput.value,
+        authorInput.value
+    );
+    createBook(newBook)
+    titleInput.value = '';
+    authorInput.value = ''
 }
