b09415b382d82c1faa79b42419e1c8156957ab3c
Nyame-Wolf <mumenyam@gmail.com>
2022-06-28 01:00:47 +0300

    Fix method to add books

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-const bookForm = document.getElementById('book-form');
-const bookContainer = document.querySelector('.book-container');
+const bookForm = document.querySelector('.book-form')
+const bookContainer = document.querySelector('.book-container')
 const titleInput = bookForm['title']
 const authorInput = bookForm['author']
 
 const books = JSON.parse(localStorage.getItem("books")) || []
 
-const addBook = (title,author) => {
+const addBook = (title, author) => {
     books.push({
         title,
-        author
-    });
+        author,
+    })
     localStorage.setItem("books", JSON.stringify(books))
     return { title, author }
 }
 
-const createBook = ({ title, author, button, hr }) => {
-    const bookDiv = document.createElement('div');
-    const bookTitle = document.createElement('p');
-    const bookAuthor = document.createElement('p');
-    const removeBtn = document.createElement('button');
-    const formHr = document.createElement('hr');
+const createBook = ({ title, author, removeBtn, hr }, index) => {
+    const bookDiv = document.createElement('div')
+    const bookTitle = document.createElement('h2')
+    const bookAuthor = document.createElement('p')
+    const AremoveBtn = document.createElement('button')
+    const Ahr = document.createElement('hr')
+
+
 
     bookTitle.innerText = `${title}`
     bookAuthor.innerText = `${author}`
-    removeBtn.innerText = `Remove`
+    AremoveBtn.innerText = `Remove`
 
-    bookDiv.append(bookTitle, bookAuthor, removeBtn, formHr);
-    bookContainer.appendChild(bookDiv);
+    bookDiv.append(bookTitle, bookAuthor, AremoveBtn, Ahr)
+    bookContainer.appendChild(bookDiv)
+
 }
 
 books.forEach(createBook)
@@ -39,6 +42,6 @@ bookForm.onsubmit = (e) => {
         authorInput.value
     );
     createBook(newBook)
-    titleInput.value = '';
-    authorInput.value = ''
+    titleInput.value = ""
+    authorInput.value = ""
 }
