a4e8e11c1c8b4e98b1da7359756a5422491c8612
Nyame-Wolf <mumenyam@gmail.com>
2022-06-28 01:37:45 +0300

    Fix linter errors, fix remove method by adding prevent default

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
-const bookForm = document.querySelector('.book-form')
-const bookContainer = document.querySelector('.book-container')
-const titleInput = bookForm['title']
-const authorInput = bookForm['author']
+const bookForm = document.querySelector('.book-form');
+const bookContainer = document.querySelector('.book-container');
+const titleInput = bookForm.title;
+const authorInput = bookForm.author;
 
-const books = JSON.parse(localStorage.getItem("books")) || []
+const books = JSON.parse(localStorage.getItem('books')) || [];
 
 const addBook = (title, author) => {
-    books.push({
-        title,
-        author,
-    })
-    localStorage.setItem("books", JSON.stringify(books))
-    return { title, author }
-}
+  books.push({
+    title,
+    author,
+  });
+  localStorage.setItem('books', JSON.stringify(books));
+  return { title, author };
+};
 
-const createBook = ({ title, author, removeBtn, hr }, index) => {
-    const bookDiv = document.createElement('div')
-    const bookTitle = document.createElement('h2')
-    const bookAuthor = document.createElement('p')
-    const AremoveBtn = document.createElement('button')
-    const Ahr = document.createElement('hr')
+const createBook = ({
+  title, author,
+}, index) => {
+  const bookDiv = document.createElement('div');
+  const bookTitle = document.createElement('h2');
+  const bookAuthor = document.createElement('p');
+  const AremoveBtn = document.createElement('button');
+  const Ahr = document.createElement('hr');
 
+  bookTitle.innerText = `${title}`;
+  bookAuthor.innerText = `${author}`;
+  AremoveBtn.innerText = 'Remove';
 
+  bookDiv.append(bookTitle, bookAuthor, AremoveBtn, Ahr);
+  bookContainer.appendChild(bookDiv);
 
-    bookTitle.innerText = `${title}`
-    bookAuthor.innerText = `${author}`
-    AremoveBtn.innerText = `Remove`
+  AremoveBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const remaining = books.filter((book, bookIndex) => bookIndex !== index);
+    localStorage.setItem('books', JSON.stringify(remaining));
+    bookDiv.remove();
+  });
+};
 
-    bookDiv.append(bookTitle, bookAuthor, AremoveBtn, Ahr)
-    bookContainer.appendChild(bookDiv)
-
-    AremoveBtn.addEventListener('click', (e) => {
-        const remainingBooks = books.filter((book, bookIndex) => {
-            return bookIndex !== index
-        })
-        localStorage.setItem("books", JSON.stringify(remainingBooks))
-        bookDiv.remove()
-    })
-
-}
-
-books.forEach(createBook)
+books.forEach(createBook);
 
 bookForm.onsubmit = (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const newBook = addBook(
-        titleInput.value,
-        authorInput.value
-    );
-    createBook(newBook)
-    titleInput.value = ""
-    authorInput.value = ""
-}
+  const newBook = addBook(
+    titleInput.value,
+    authorInput.value,
+  );
+  createBook(newBook);
+  titleInput.value = '';
+  authorInput.value = '';
+};
