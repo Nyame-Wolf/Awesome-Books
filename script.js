90787ca87936f0e85b4af0e0385d5ca63bfe1e1c
DavyKK <davykk04@gmail.com>
2022-06-27 23:06:20 +0300

    create JS oject

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ const books = [
 
 const addBook = (author, book) => { }
 
-const createBook = () => {
-    const bookDiv = document.createElement('div')
+const createBook = ({title,author,button,hr}) => {
+    const bookDiv = document.createElement('div');
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    const removeBtn = document.createElement('button');
+    const formHr = document.createElement('hr');
 
+    bookTitle.innerText = `${title}`
+    bookAuthor.innerText = `${author}`
+    removeBtn.innerText = `Remove`
 
+    bookDiv.append(bookTitle,bookAuthor,removeBtn,formHr);
+    bookContainer.appendChild(bookDiv);
 }
