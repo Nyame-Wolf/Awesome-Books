83e6475b89fdd6735e34fb7a8347f6c3e70e4002
Nyame-Wolf <mumenyam@gmail.com>
2022-06-28 12:56:05 +0300

    Add second parameter to createBook method to remove particular item

--- script.js
+++ script.js
@@ -6,48 +6,50 @@ const authorInput = bookForm.author;
 const books = JSON.parse(localStorage.getItem('books')) || [];
 
 const addBook = (title, author) => {
-  books.push({
-    title,
-    author,
-  });
-  localStorage.setItem('books', JSON.stringify(books));
-  return { title, author };
+    books.push({
+        title,
+        author,
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+    return { title, author };
 };
 
 const createBook = ({
-  title, author,
+    title, author,
 }, index) => {
-  const bookDiv = document.createElement('div');
-  const bookTitle = document.createElement('h2');
-  const bookAuthor = document.createElement('p');
-  const AremoveBtn = document.createElement('button');
-  const Ahr = document.createElement('hr');
+    const bookDiv = document.createElement('div');
+    const bookTitle = document.createElement('h2');
+    const bookAuthor = document.createElement('p');
+    const AremoveBtn = document.createElement('button');
+    const Ahr = document.createElement('hr');
 
-  bookTitle.innerText = `${title}`;
-  bookAuthor.innerText = `${author}`;
-  AremoveBtn.innerText = 'Remove';
+    bookTitle.innerText = `${title}`;
+    bookAuthor.innerText = `${author}`;
+    AremoveBtn.innerText = 'Remove';
 
-  bookDiv.append(bookTitle, bookAuthor, AremoveBtn, Ahr);
-  bookContainer.appendChild(bookDiv);
+    bookDiv.append(bookTitle, bookAuthor, AremoveBtn, Ahr);
+    bookContainer.appendChild(bookDiv);
 
-  AremoveBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    books.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(books));
-    bookDiv.remove();
-  });
+    AremoveBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        books.splice(index, 1);
+        console.log(index)
+        console.log(index - 1)
+        localStorage.setItem('books', JSON.stringify(books));
+        bookDiv.remove();
+    });
 };
 
 books.forEach(createBook);
 
 bookForm.onsubmit = (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const newBook = addBook(
-    titleInput.value,
-    authorInput.value,
-  );
-  createBook(newBook);
-  titleInput.value = '';
-  authorInput.value = '';
+    const newBook = addBook(
+        titleInput.value,
+        authorInput.value,
+    );
+    createBook(newBook, books.length - 1);
+    titleInput.value = '';
+    authorInput.value = '';
 };
