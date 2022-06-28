1624b6e47a5d2a35ef6ba30b555e48268e91dbcf
Nyame-Wolf <mumenyam@gmail.com>
2022-06-28 18:46:45 +0300

    Refactor to remove specific book

--- script.js
+++ script.js
@@ -3,66 +3,62 @@ const bookContainer = document.querySelector('.book-container');
 const titleInput = bookForm.title;
 const authorInput = bookForm.author;
 
-let books = JSON.parse(localStorage.getItem('books')) || [];
+const books = JSON.parse(localStorage.getItem('books')) || [];
 const uniqueIdGen = () => {
-    let id = 0
-    return () => ++id
-}
-const uniqueId = uniqueIdGen()
+  let id = Date.now();
+  // eslint-disable-next-line no-return-assign
+  return () => id += 1;
+};
+const uniqueId = uniqueIdGen();
 const addBook = (title, author) => {
-    books.push({
-        title,
-        author,
-        id: uniqueId(),
-    });
-    localStorage.setItem('books', JSON.stringify(books));
-    return { title, author };
+  const id = uniqueId();
+  books.push({
+    title,
+    author,
+    id,
+  });
+  localStorage.setItem('books', JSON.stringify(books));
+  return { title, author, id };
 };
 
 const createBook = ({
-    title, author, id
+  title, author, id,
 }) => {
-    const bookDiv = document.createElement('div');
-    bookDiv.setAttribute('data-id', id);
-    const bookTitle = document.createElement('h2');
-    const bookAuthor = document.createElement('p');
-    const AremoveBtn = document.createElement('button');
-    AremoveBtn.classList.add('remove')
-    const Ahr = document.createElement('hr');
+  const bookDiv = document.createElement('div');
+  bookDiv.setAttribute('data-id', id);
+  const bookTitle = document.createElement('h2');
+  const bookAuthor = document.createElement('p');
+  const AremoveBtn = document.createElement('button');
+  AremoveBtn.classList.add('remove');
+  const Ahr = document.createElement('hr');
 
-    bookTitle.innerText = `${title}`;
-    bookAuthor.innerText = `${author}`;
-    AremoveBtn.innerText = 'Remove';
+  bookTitle.innerText = `${title}`;
+  bookAuthor.innerText = `${author}`;
+  AremoveBtn.innerText = 'Remove';
 
-    bookDiv.append(bookTitle, bookAuthor, AremoveBtn, Ahr);
-    bookContainer.appendChild(bookDiv);
+  bookDiv.append(bookTitle, bookAuthor, AremoveBtn, Ahr);
+  bookContainer.appendChild(bookDiv);
 
-    // AremoveBtn.addEventListener('click', (e) => {
-    //     e.preventDefault();
-    //     books.splice(index, 1);
-    //     localStorage.setItem('books', JSON.stringify(books));
-    //     localStorage.removeItem('bookDiv')
-    //     bookDiv.remove();
-    // });
-};
-bookContainer.addEventListener('click', (e) => {
-    const bookId = e.target.parentElement.dataset.id
-    const bookIndex = books.findIndex((book) => book.id = bookId)
-    books.splice(bookIndex, 1);
+  AremoveBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const index = books.findIndex((book) => book.id === id);
+    books.splice(index, 1);
     localStorage.setItem('books', JSON.stringify(books));
-    e.target.parentElement.remove();
-})
+    localStorage.removeItem('bookDiv');
+    bookDiv.remove();
+  });
+};
 
 books.forEach(createBook);
 
 bookForm.onsubmit = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const newBook = addBook(
-        titleInput.value,
-        authorInput.value,
-    );
-    createBook(newBook);
-    titleInput.value = '';
-    authorInput.value = '';
+  const newBook = addBook(
+    titleInput.value,
+    authorInput.value,
+  );
+  createBook(newBook);
+  titleInput.value = '';
+  authorInput.value = '';
 };
